75faa6841d74a8825ac26127fb3f68bc6d4a7b63
Peder Hatlen <pehaa002@osloskolen.no>
2022-09-07 20:51:50 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let hexClockEl = document.getElementById("HexClock");
+let hexTimer;
+
+let width;
+let height;
+
+function setup(){
+	width = document.body.scrollWidth;
+	height = document.body.scrollHeight;
+
+	let hyp = Math.sqrt((width**2)+(height**2));
+	let deg = 180 + Math.acos(width/hyp)*(180/Math.PI);
+	// console.log(width, height, hyp, deg);
+
+	document.documentElement.style.setProperty('--rotation', deg + "deg");
+}
+function hexClock(){
+	const date = new Date();
+	let time = [date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds()];
+	let hex = "#"+('000000' + Math.round((16777216/86400000)*(time[0]*3600000 + time[1]*60000 + time[2]*1000 + time[3])).toString(16).toUpperCase()).slice(-6);
+	hexClockEl.style.color = hex;
+}
+
+hexClockEl.addEventListener("mouseover", (e)=>{
+	hexClock();
+	hexTimer = setInterval(hexClock, 250);
+});
+hexClockEl.addEventListener("mouseout", (e)=>{
+	clearInterval(hexTimer);
+	hexClockEl.style.color = "";
+});
+window.addEventListener("resize", setup);
+window.addEventListener("load", setup);
